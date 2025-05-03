44c011d0fe2bab2221df0436bd454fec2e705a3e
Pandaman <147335739+Pandaman08@users.noreply.github.com>
2025-05-02 23:05:18 -0500

    Rol por Ususario

---
+++ dao/RolDAO.js
@@ -0,0 +1,23 @@
+const db='../config/db.js';
+
+class RolDAO {
+    static async createRol(nombreRol) {
+        try {
+            const query = 'INSERT INTO rol (NombreRol) VALUES (?)';
+            const [result] = await db.execute(query, [nombreRol]);
+            return { id: result.insertId, nombreRol };
+        } catch (error) {
+            throw error.status ? error : new Error('Error al crear el rol');
+        }
+    }
+
+    static async getRolById(idRol) {
+        try {
+            const query = 'SELECT * FROM rol WHERE idRol = ?';
+            const [rows] = await db.execute(query, [idRol]);
+            return rows;
+        } catch (error) {
+            throw error.status ? error : new Error('Error al obtener el rol');
+        }
+    }
+}
