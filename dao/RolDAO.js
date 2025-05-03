10a3094ac109dcbe48527d0703077e617172f594
ACastroAnalytia <acastro@analytia.pe>
2025-05-02 23:15:37 -0500

    module.export rol dao

--- dao/RolDAO.js
+++ dao/RolDAO.js
@@ -13,11 +13,14 @@ class RolDAO {
 
     static async getRolById(idRol) {
         try {
+            console.log(idRol);
             const query = 'SELECT * FROM rol WHERE idRol = ?';
             const [rows] = await db.execute(query, [idRol]);
+            console.log(rows);
             return rows;
         } catch (error) {
             throw error.status ? error : new Error('Error al obtener el rol');
         }
     }
 }
+module.exports = RolDAO;
