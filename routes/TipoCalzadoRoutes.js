3ef6f23299e94f1a0295fe5cf3b82716fcdfbf32
AJoelCastro <misthorn33@gmail.com>
2025-05-03 18:14:13 -0500

    changes

--- routes/TipoCalzadoRoutes.js
+++ routes/TipoCalzadoRoutes.js
@@ -3,7 +3,7 @@ const tipoCalzadoController = require('../controllers/TipoCalzadoController')
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
-router.post('/crear',tipoCalzadoController.createTipoModelo);
+router.post('/crear', authMiddleware,tipoCalzadoController.createTipoModelo);
 router.get('/todos',authMiddleware,tipoCalzadoController.getAllTipoCalzado);
 router.get('/nombre',authMiddleware,tipoCalzadoController.getTipoCalzadoByNombre);
 router.get('/obtener/:codigoPedido',authMiddleware,tipoCalzadoController.getTipoCalzadoByCodigoPedido);
